e17cc222d6805e90a8c11a76f190eaa8dd27c72b
SerZin <zinoviev.serj@gmail.com>
2025-03-10 20:55:45 +0100

    h\w-js7

---
+++ js/task-3.js
@@ -0,0 +1,13 @@
+
+const nameInput = document.getElementById('name-input');
+const nameOutput = document.getElementById('name-output');
+
+
+nameInput.addEventListener('input', () => {
+  const trimmedName = nameInput.value.trim();
+  if (trimmedName === '') {
+    nameOutput.textContent = 'Anonymous'; 
+  } else {
+    nameOutput.textContent = trimmedName;
+  }
+});
---
+++ js/task-5.js
@@ -0,0 +1,18 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+const changeColorButton = document.querySelector('.change-color');
+const colorSpan = document.querySelector('.color');
+
+changeColorButton.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = `rgb(${parseInt(randomColor.slice(1, 3), 16)}, ${parseInt(randomColor.slice(3, 5), 16)}, ${parseInt(randomColor.slice(5, 7), 16)})`;
+  colorSpan.textContent = randomColor;
+});
